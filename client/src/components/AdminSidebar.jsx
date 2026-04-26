41a64db0b95a3d51a64ad7a2548a1ce7ad2bfe9c
Ali ahmed <aa5325368@gmail.com>
2026-04-26 23:48:42 +0300

    test

--- client/src/components/AdminSidebar.jsx
+++ client/src/components/AdminSidebar.jsx
@@ -6,7 +6,7 @@ import api from '../utils/api';
 
 const AdminSidebar = () => {
   const navigate = useNavigate();
-
+const test = 0;
     const logout = async (e) => {
         e.preventDefault();
 
