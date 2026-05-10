569e28deb5ab08ec00ddf58fea1c2523e0b88e1e
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-05-11 01:47:31 +0300

    added loading skeleton for image, made sure (Become A Member) button does not show if LoggedIn

--- client/src/sections/events/EventsHero.jsx
+++ client/src/sections/events/EventsHero.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 
-import HeroImage from '../../assets/images/events/events-hero.jpg';
+// Components
 import Button from '../../components/Button';
- 
+import ImageSkeleton from '../../components/ImageSkeleton.jsx';
+// Images
+import HeroImage from '../../assets/images/events/events-hero.jpg';
+import { useAuth } from '../../context/AuthContext';
+				
 export default function EventsHero() {
+  const { user } = useAuth();
+	const [imgLoaded, setImgLoaded] = useState(false);
+	
 	return <section id="events-hero" className="py-10 min-h-[calc(100vh-var(--navbar-height))] grid place-items-center gap-20 grid-cols-1 lg:grid-cols-2 text-center md:text-left max-w-7xl mx-auto">
 
 		  {/* Bottom shadow overlay */}
@@ -25,31 +32,38 @@ export default function EventsHero() {
 				</p>
 
 				{/* Call-To-Action Buttons*/}
-				<div className='*:text-lg *:lg:text-xl flex gap-3 justify-center md:justify-start mt-5 *:px-6 *:py-3 lg:*:px-12 lg:*:py-6 *:rounded-xl'>
-					<button
-						className='font-light text-primary hover:bg-white/40 hover:text-primary-dark dark:text-white hover:dark:text-white dark:bg-[#2563EB] hover:dark:bg-[#2563EB]/60 transition duration-300'
-						style={{ boxShadow: '0px 0px 12px #00C0E8B2, 0px 10px 15px #0000001A, 0px 4px 6px #0000001A' }}
-					>
-							<Link to='/'>
-									Explore Events
+				<div className='*:text-lg *:lg:text-xl flex gap-3 justify-center md:justify-start mt-5'>
+					<a href='#upcoming-events'>
+						<button
+							className='px-6 py-3 lg:px-12 lg:py-6 w-full rounded-xl font-light text-primary hover:bg-white/40 hover:text-primary-dark dark:text-white hover:dark:text-white dark:bg-[#2563EB] hover:dark:bg-[#2563EB]/60 transition duration-300'
+							style={{ boxShadow: '0px 0px 12px #00C0E8B2, 0px 10px 15px #0000001A, 0px 4px 6px #0000001A' }}
+						>
+							Explore Events
+						</button>
+					</a>
+					{
+						!user && (
+							<Link to='/login'>
+								<Button className='w-full rounded-xl px-6 py-3 lg:px-12 lg:py-6 bg-linear-to-r from-primary to-primary-light hover:from-primary-light hover:to-primary transition text-white dark:hover:bg-white/10 hover:text-white dark:bg-main dark:from-transparent dark:to-transparent hover:dark:from-transparent hover:dark:to-transparent dark:border dark:border-border'>
+											Become a Member
+								</Button>
 							</Link>
-					</button>
-					{/* bg-primary-linear*/}
-					<Button className='bg-linear-to-r from-primary to-primary-light hover:from-primary-light hover:to-primary transition text-white dark:hover:bg-white/10 hover:text-white dark:bg-main dark:from-transparent dark:to-transparent hover:dark:from-transparent hover:dark:to-transparent dark:border dark:border-border'>
-						<Link to='/signup'>
-								Become a Member
-						</Link>
-					</Button>
+						)
+					}
 				</div>
 
 			</div>
 
 			{/* Right Section - images & card */}
 			<div className='mx-10 md:mx-0 rounded-2xl shadow-[0px_10px_30px_0px_#2563EB1F] dark:shadow-[0px_0px_40px_0px_#2563EB33]rounded-3xl dark:bg-[#0F172ACC] border border-[#E2E8F0]/8'>
-				<img
-				  src={HeroImage}
-				  className="w-full h-full object-cover rounded-3xl"
-				/>
+				<div className='relative'>
+					{!imgLoaded && <ImageSkeleton />}
+					<img
+					  src={HeroImage}
+						onLoad={() => setImgLoaded(true)}
+					  className="w-full h-full object-cover rounded-3xl"
+					/>
+				</div>
 				<div className='flex flex-col gap-6 py-7 px-5'>
 					{/* Badge */}
 					<div className='text-[10px] lg:text-sm text-[#2563EB] bg-[#2563EB]/20 dark:text-primary-light dark:bg-primary-light/20 rounded-full w-fit px-3.5 py-2'>
