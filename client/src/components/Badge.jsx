e9f81069924d6e49e4c4f970360f2c15281d2bf5
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-06-22 22:10:02 +0300

    style: updated colors

--- client/src/components/Badge.jsx
+++ client/src/components/Badge.jsx
@@ -3,9 +3,9 @@ import clsx from 'clsx'
 
 export default function Badge({ text, className }) {
   return (
-		<span className={clsx(`bg-primary/10 text-primary-dark dark:text-primary
-	    border border-primary-dark dark:border-primary
-	    rounded-full px-8 py-1 text-xs lg:text-sm uppercase`,
+		<span className={clsx(`text-primary dark:text-primary-light bg-primary/10
+	    border border-primary-dark/10 dark:border-primary-light/10
+	    rounded-full px-8 py-1 text-xs uppercase`,
 			className)}
 		>
       {text}
