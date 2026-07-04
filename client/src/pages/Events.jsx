2af4f441dbdedb89d09c2299f7608c17d8b56544
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-07-04 11:36:26 +0300

    refactor: passing data brackets to upcoming and previous events to display them

--- client/src/pages/Events.jsx
+++ client/src/pages/Events.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { usePublicEvents } from '../hooks/usePublicEvents'
+import { useAuth } from '../context/AuthContext'
+import api from '../utils/api'
 
 import EventsHero from '../sections/events/EventsHero'
 import UpcomingEvents from '../sections/events/UpcomingEvents'
 import PreviousEvents from '../sections/events/PreviousEvents'
 
 const Events = () => {
+  const { user } = useAuth()
+  const [registeredFormIds, setRegisteredFormIds] = useState(new Set())
   const { upcoming, previous, loading, error } = usePublicEvents()
+
+  useEffect(() => {
+    if (!user?._id) {
+      setRegisteredFormIds(new Set())
+      return
+    }
+    api
+      .get(`/users/${user._id}/events`)
+      .then((res) => {
+        const submissions = res.data?.data || []
+        setRegisteredFormIds(new Set(submissions.map((s) => s.formId)))
+      })
+      .catch(() => setRegisteredFormIds(new Set()))
+  }, [user?._id])
+
+  const markRegistered = (events) =>
+    events.map((e) => ({
+      ...e,
+      isRegistered: registeredFormIds.has(e.formId),
+    }))
 
   return (
     <main>
       <EventsHero featuredEvent={upcoming[0]} loading={loading} />
-      <UpcomingEvents events={upcoming} loading={loading} />
-      <PreviousEvents events={previous} loading={loading} />
+      <UpcomingEvents events={markRegistered(upcoming)} loading={loading} />
+      <PreviousEvents events={markRegistered(previous)} loading={loading} />
     </main>
   )
 }
