cca1afcbbd522dcc17d2c8503d173e42cd7a4459
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-16 11:20:12 +0300

    refactor: move sponsors section outside of parent

---
+++ client/src/data/sponsors.js
@@ -0,0 +1,31 @@
+import codeClouders from "../assets/images/sponsers/code clouders.jpeg";
+import edges from "../assets/images/sponsers/edges.jpeg";
+import eduvate from "../assets/images/sponsers/EDUVATE.jpg";
+import emas from "../assets/images/sponsers/EMAS.png";
+import haLogo from "../assets/images/sponsers/HA logo.png";
+import jetsolar from "../assets/images/sponsers/jet-solar.png";
+import tramAcademy from "../assets/images/sponsers/tram-academy.JPG";
+import kian from "../assets/images/sponsers/kian.jpeg";
+import neo from "../assets/images/sponsers/neo (1).jpg";
+import porto from "../assets/images/sponsers/PORTO (1).jpg";
+import seweddy from "../assets/images/sponsers/SEWEDY (1).jpg";
+import ult from "../assets/images/sponsers/ULT.jpg";
+import itc from "../assets/images/sponsers/itc.JPG";
+import MeLed from "../assets/images/sponsers/me-led.JPG";
+
+export const SPONSORS = [
+  { src: codeClouders, alt: "Code Clouders" },
+  { src: edges, alt: "Edges" },
+  { src: eduvate, alt: "Eduvate" },
+  { src: emas, alt: "EMAS" },
+  { src: haLogo, alt: "HA Logo" },
+  { src: jetsolar, alt: "JET SOLAR" },
+  { src: tramAcademy, alt: "TRAM Academy" },
+  { src: kian, alt: "Kian" },
+  { src: neo, alt: "Neo" },
+  { src: porto, alt: "Porto" },
+  { src: seweddy, alt: "Sewedy" },
+  { src: ult, alt: "ULT" },
+  { src: itc, alt: "ITC" },
+  { src: MeLed, alt: "ME-LED" },
+];
